f6789159438784727d3b3ed60731415e61a35df0
Aaron Bieber <aaron@bolddaemon.com>
2018-03-31 16:33:17 -0600

    check for sig and asc when fetching and verifying
    - add target for removing signature files
    - exit properly on sig failure.

--- signing.mk
+++ signing.mk
@@ -1,31 +1,52 @@
 # One of gnupg, signify, opmsg
 SIG_TYPE ?=	gnupg
-# Typically .asc for detached signatures
 SIG_SUFX ?=	.asc
-SIG_FILE ?=	${DISTNAME}${EXTRACT_SUFX}${SIG_SUFX}
+SIG_FILE ?=	${DISTNAME}${EXTRACT_SUFX}
 SIG_PATH ?=	${DISTDIR}/${SIG_FILE}
+
+clean-sig:
+	rm -f ${SIG_PATH}{.sig,.asc}
 
 fetch-sig:
 	@${ECHO_MSG} "===> Checking signature files for ${FULLPKGNAME}${_MASTER}"
-	@if [ ! -f ${SIG_PATH} ]; then \
-	  for site in ${MASTER_SITES}; do \
-	    if ${_PFETCH} ${FETCH_CMD} -o ${SIG_PATH} $${site}${SIG_FILE}; then \
-	      exit 0; \
-	    fi; \
-	  done; exit 1; \
-	fi
+	@for sig in ${SIG_SUFX} .asc .sig; do \
+		if [ ! -f ${SIG_PATH}${SIG_SUFX} ] && \
+			[ ! -f ${SIG_PATH}.asc ] && \
+			[ ! -f ${SIG_PATH}.sig ]; then \
+			for site in ${MASTER_SITES}; do \
+				if ${_PFETCH} ${FETCH_CMD} -o ${SIG_PATH}$${sig} \
+					$${site}${SIG_FILE}$${sig}; then \
+					exit 0; \
+				else \
+					break; \
+				fi; \
+			done; \
+		else \
+			exit 0; \
+		fi ; \
+	done ; exit 1;
 
 verify: fetch fetch-sig
 	@if [ ${SIG_TYPE} == "gnupg" ]; then \
-		env GNUPGHOME=${PREFIX}/share/signing/gnupg/ \
-			${PREFIX}/bin/gpg2 --verify \
-			${SIG_PATH} && \
-		echo "Signature OK" || \
-		echo "Signature NOT VALID!" ; \
+		if [ -f ${SIG_PATH}.asc ]; then \
+			env GNUPGHOME=${PREFIX}/share/signing/gnupg/ \
+				${PREFIX}/bin/gpg2 --verify \
+				${SIG_PATH}.asc && \
+			( echo "Signature OK"; exit 0 ) || \
+			( echo "Signature NOT VALID!"; exit 1 ) ; \
+		else \
+			env GNUPGHOME=${PREFIX}/share/signing/gnupg/ \
+				${PREFIX}/bin/gpg2 --verify \
+				${SIG_PATH}.sig && \
+			( echo "Signature OK"; exit 0 )|| \
+			( echo "Signature NOT VALID!"; exit 1 ) ; \
+		fi ; \
 	fi;
 	@if [ ${SIG_TYPE} == "opmsg" ]; then \
 		${PREFIX}/bin/opmsg -c \
 			${PREFIX}/share/signing/opmsg \
-			-V ${DISTDIR}/${DISTNAME}${EXTRACT_SUFX} \
-			--in ${SIG_PATH} ; \
+				-V ${DISTDIR}/${DISTNAME}${EXTRACT_SUFX} \
+				--in ${SIG_PATH}.sig && \
+		( echo "Signature OK"; exit 0 ) || \
+		( echo "Signature NOT VALID!"; exit 1 ); \
 	fi;
